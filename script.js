9a93f315f10a1f54ff802f368098f9ff3153fbb6
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2022-01-21 17:12:12 +0200

    Final-4

--- script.js
+++ script.js
@@ -48,73 +48,7 @@ let correctWordOfAge = function(age){
     else {
         result = 'лет!';
     }
-    /* if(age%10 == 1 && age != 11){
-        result = 'год!';
-    }
-    else if((age>1 && age<5) || (age>20 && (age%10==2 || age%10==3 || age%10==4))){
-        result = 'года!';
-    }
-    else{
-        result = 'лет!';
-    } */
     return result;
 }
 
 getAge();
-
-
-
-
-
-
-
-
-
-
-/*let getAge = function(){
-    let userName = prompt('Введите свое имя');    
-    let message = 'Введите свой год рождения';
-    let userYear = prompt(message);
-    while(1){    
-        if(userYear>1900 && userYear < 2022){
-            break;
-        }
-        else{
-            alert('Неправильный год рождения!')
-            message = 'Введите ВЕРНЫЙ год рождения';
-            userYear = prompt(message);
-        }
-    }
-    let userAge = howOld(2022, userYear);
-    console.log(userAge);
-    confirm(userName + ', вам '+ userAge + ' ' + correctWordOfAge(userAge) + '?');
-}
-
-let howOld = function(thisYear, yearOfBirthday){
-    return thisYear-yearOfBirthday;
-}
-
-let correctWordOfAge = function(age){
-    let result = '';
-    if(age%10 == 1){
-        result = 'год';
-    }
-    else if((age>1 && age<5) || (age>20 && (age%10==2 || age%10==3 || age%10==4))){
-        result = 'года';
-    }
-    else{
-        result = 'лет';
-    }
-    return result;
-}
-
-getAge();*/
-
-/*
-1)Запросить у пользователя ввести имя и год рождения (в 2-ух разных окнах)
-2)Проверить что год введен корректно
-3)Высчитать сколько пользователю лет
-4)Сформировать корректную строку для вопроса из высчитанного 
-значения сколько пользователю лет (Возможные варианты окончания
- строки: год, года, лет. Пример - "Вадим, вам 24 года?")
-5)Вывести эту строку для подтверждения (в confirm)*/
